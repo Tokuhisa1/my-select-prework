12b7246a7051c40ea3e01e91244996978b92821d
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-07-10 17:25:25 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # Your code here!
+ collection2 = []
+ i = 0
+
+ while i < collection.length
+   collection2.push(yield(collection[i]))
+   i = i + 1
+ end
+
+ collection2
 end
